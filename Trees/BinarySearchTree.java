f284be6f7a1271b28c5a853261a36c77a0ec25a7
AmadouSallah <amadousallah13@gmail.com>
2018-03-01 01:13:01 -0800

    Added insert method for BinarySearchTree.java

--- Trees/BinarySearchTree.java
+++ Trees/BinarySearchTree.java
@@ -1,5 +1,41 @@
 public class BinarySearchTree {
-  
+
+    public Node root;
+    int size;
+
+    public BinarySearchTree() {
+      root = null;
+      size = 0;
+    }
+
+    public void insert(int value) {
+      Node newNode = new Node(value);
+      if (root == null) {
+        root = newNode;
+        size++;
+        return;
+      }
+
+      Node current = root, parent;
+      while (current != null) {
+        parent = current;
+        if (value < current.data) {
+          current = current.left;
+          if (current == null) {
+            parent.left = newNode;
+            break;
+          }
+        } else {
+          current = current.right;
+          if (current == null) {
+            parent.right = newNode;
+            break;
+          }
+        }
+      }
+      size++;
+    }
+
   private class Node {
     int data;
     Node left, right;
@@ -8,4 +44,15 @@ public class BinarySearchTree {
       this.data = data;
     }
   }
+
+  public static void main(String[] args) {
+    BinarySearchTree bst = new BinarySearchTree();
+    bst.insert(3);
+    bst.insert(5);
+    bst.insert(1);
+    bst.insert(6);
+    bst.insert(2);
+    bst.insert(0);
+  }
+
 }
