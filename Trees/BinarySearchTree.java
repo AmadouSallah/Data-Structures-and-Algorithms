5237010f3ebe79c99976b73afedff031ecff94c9
AmadouSallah <amadousallah13@gmail.com>
2018-03-01 01:51:38 -0800

    Added getMin method for BinarySearchTree.java

--- Trees/BinarySearchTree.java
+++ Trees/BinarySearchTree.java
@@ -37,13 +37,13 @@ public class BinarySearchTree {
     }
 
 
-    public static void inorderTraversal(Node node) {
+    public static void printInorderTraversal(Node node) {
       if (node == null) {
         return;
       }
-      inorderTraversal(node.left);
+      printInorderTraversal(node.left);
       System.out.print(node.data + " ");
-      inorderTraversal(node.right);
+      printInorderTraversal(node.right);
     }
 
   private static class Node {
@@ -55,6 +55,14 @@ public class BinarySearchTree {
     }
   }
 
+  public static int getMin(Node node) {
+    Node current = node;
+    while (current.left != null) {
+      current = current.left;
+    }
+    return current.data;
+  }
+
   public static void main(String[] args) {
     BinarySearchTree bst = new BinarySearchTree();
     bst.insert(3);
@@ -64,8 +72,10 @@ public class BinarySearchTree {
     bst.insert(2);
     bst.insert(0);
 
-    inorderTraversal(bst.root);
+    printInorderTraversal(bst.root);
     System.out.println();
+
+    System.out.println("minimum value = " + bst.getMin(bst.root));
   }
 
 }
